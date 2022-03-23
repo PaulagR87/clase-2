a1b9e54793d586381725102aab1eba137287995d
unknown <paula.g.rodriguez@gmail.com>
2022-03-23 09:46:00 -0300

    Entrega 1

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hola <code>Mi nombre </code> es Paula Rodriguez.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://paulagr87.github.io/Porfoliomine/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Porfolio creado con lo aprendido en CoderHouse
         </a>
       </header>
     </div>
